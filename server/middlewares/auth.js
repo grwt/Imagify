393f04adf0e613755563796e28303747729dc76b
Ganesh Singh Rawat <150654743+grwt@users.noreply.github.com>
2025-07-24 17:58:07 +0530

    Update auth.js

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,19 +1,15 @@
-
 import jwt from 'jsonwebtoken';
 
 const userAuth = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
+  // Add console warning here for debugging
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    console.warn('Authorization header missing or improperly formatted:', authHeader);
     return res.status(403).json({ success: false, message: 'Not authorized. Login again.' });
-
-
-
   }
 
-
-
-  const token = authHeader.split(' ')[1]; // extract the token part
+  const token = authHeader.split(' ')[1]; // Extract the token part
 
   try {
     const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
@@ -26,11 +22,6 @@ const userAuth = async (req, res, next) => {
     }
   } catch (error) {
     return res.status(403).json({ success: false, message: error.message });
-
-
-
-
-
   }
 };
 
