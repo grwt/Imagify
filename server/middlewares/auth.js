5c21f91d5568ccef0c09a6cdba19de23dd9b41f4
Ganesh Singh Rawat <150654743+grwt@users.noreply.github.com>
2025-07-24 17:08:09 +0530

    Update auth.js

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,24 +1,26 @@
-const userAuth = async (req, res, next) => {
-  const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(403).json({ success: false, message: 'Not authorized. Login again.' });
+import jwt from 'jsonwebtoken';
+
+const userAuth=async (req, res, next) => {
+  const {token}=req.headers;
+   
+  if(!token){
+    return res.json({sucess:false ,message:'Not Authorzed,Login Again'});
   }
+  try{
+    const tokenDecode=jwt.verify(token,process.env.JWT_SECRET);
 
-  const token = authHeader.split(' ')[1]; // extract the token part
-
-  try {
-    const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
-
-    if (tokenDecode.id) {
+    if(tokenDecode.id){
       req.user = { id: tokenDecode.id };
-      next();
-    } else {
-      return res.status(403).json({ success: false, message: 'Invalid token' });
     }
-  } catch (error) {
-    return res.status(403).json({ success: false, message: error.message });
+    else{
+      return res.json({sucess:false ,message:'Not Authorized, login again'});
+    }
+    next();
   }
-};
+  catch(error){
+    return res.json({sucess:false ,message:error.message});
+  }
+}
 
 export default userAuth;
