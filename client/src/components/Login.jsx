7f62e0cff5a83d40fcb8206c491006c0e434e80d
Ganesh Singh Rawat <105rawawthimanshu@gmail.com>
2025-07-24 12:57:18 +0530

    fixes typos

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -83,7 +83,7 @@ const Login = () => {
 
         <p className='text-sm text-blue-600 my-4 cursor-pointer'>Forgot Password?</p>
 
-        <button className='bg-blue-600 w-full text-white py-2 rounded-full'> {state==='Login' ? 'login': 'create account'} create account</button>
+        <button className='bg-blue-600 w-full text-white py-2 rounded-full'> {state==='Login' ? 'login': 'create account'} </button>
 
         
         {state=='Login' ? <p className='mt-5 text-center'>Don't have an account? <span className='text-blue-600 cursor-pointer' onClick={()=>setState('Sign Up')}>Sign up</span></p>
